7c7b7aff461995395f8b193c9c029135c480ed29
Gianni <58817136+GianniGithub@users.noreply.github.com>
2021-06-02 00:46:04 +0200

    First Working Demo as Go template

---
+++ Class1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Blochub_API_C_sharp
+{
+    public class BlocStream
+    {
+        private readonly string serverUrl;
+        private ClientWebSocket socket;
+        private BlocWebSocketStream stream;
+
+        /// <summary>
+        /// Gets the process ID of the server process.
+        /// </summary>
+        public int ProcessId { get; private set; }
+		public bool IsConnected { get; private set; }
+
+		/// <summary>
+		/// Initializes an instance of the WebsocketClientChannel.
+		/// </summary>
+		/// <param name="url">The full path to the server process executable.</param>
+		public BlocStream(string url)
+        {
+            this.serverUrl = url;
+            this.socket = new ClientWebSocket();
+            this.stream = new BlocWebSocketStream(socket);
+        }
+
+        public virtual async Task WaitForConnection()
+        {
+            try
+            {
+                await this.socket.ConnectAsync(new Uri(serverUrl), CancellationToken.None);
+            }
+            catch (AggregateException ex)
+            {
+                var wsException = ex.InnerExceptions.FirstOrDefault() as WebSocketException;
+
+                if (wsException != null)
+                {
+                    var errMas =    string.Format("Failed to connect to WebSocket server. Error was '{0}'", wsException.Message);
+
+                    // Log here
+					Console.WriteLine(errMas);
+
+                }
+
+                throw;
+            }
+
+            this.IsConnected = true;
+        }
+
+    }
+
+    /// <summary>
+    /// Extension of <see cref="MemoryStream"/> that sends data to a WebSocket during FlushAsync 
+    /// and reads during WriteAsync.
+    /// <see cref="https://www.csharpcodi.com/vs2/?source=4178/PowerShellEditorServices/src/PowerShellEditorServices.Channel.WebSocket/WebsocketServerChannel.cs"/>
+    /// </summary>
+    internal class BlocWebSocketStream : MemoryStream
+    {
+        private readonly ClientWebSocket socket;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <remarks>
+        /// It is expected that the socket is in an Open state. 
+        /// </remarks>
+        /// <param name="socket"></param>
+        public BlocWebSocketStream(ClientWebSocket socket)
+        {
+            this.socket = socket;
+        }
+
+        /// <summary>
+        /// Reads from the WebSocket. 
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="count"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            if (socket.State != WebSocketState.Open)
+            {
+                return 0;
+            }
+
+            WebSocketReceiveResult result;
+            do
+            {
+                result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer, offset, count), cancellationToken);
+            } while (!result.EndOfMessage);
+
+            if (result.MessageType == WebSocketMessageType.Close)
+            {
+                await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", cancellationToken);
+                return 0;
+            }
+
+            return result.Count;
+        }
+
+        /// <summary>
+        /// Sends the data in the stream to the buffer and clears the stream.
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public override async Task FlushAsync(CancellationToken cancellationToken)
+        {
+            await socket.SendAsync(new ArraySegment<byte>(ToArray()), WebSocketMessageType.Binary, true, cancellationToken);
+            SetLength(0);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Blochub_API_C_sharp
+{
+	class Program
+	{
+        static async Task Main(string[] args)
+		{
+			string uri = "ws://163.172.162.138:80/blocfeed";
+			string JSONcommand = @"{""type"":""subscribe"",""apikey"":""3JGKGK38D-THIS-IS-SAMPLE-KEY"",""encoding"":""json"",""markets"":[""binance"",""bitfinex""],""symbols"":[""XRP/BTC"",""BTC/EUR"",""ETH/BTC"",""ETH/EUR""],""channel"":""ticker""}";
+
+			//var sub = new BlocSubscriberSubscription() { ApiKey = "3JGKGK38D-THIS-IS-SAMPLE-KEY", Channel = }k
+
+			do
+			{
+				using (var socket = new ClientWebSocket())
+					try
+					{
+						await socket.ConnectAsync(new Uri(uri), CancellationToken.None);
+						
+						await Send(socket, JSONcommand);
+						await Receive(socket);
+
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine($"ERROR - {ex.Message}");
+					}
+			} while (true);
+
+		}
+
+
+		// see https://thecodegarden.net/websocket-client-dotnet
+		static async Task Receive(ClientWebSocket socket)
+		{
+			var buffer = new ArraySegment<byte>(new byte[2048]);
+
+			do
+			{
+				WebSocketReceiveResult result;
+				using (var ms = new MemoryStream())
+				{
+					do
+					{
+						result = await socket.ReceiveAsync(buffer, CancellationToken.None);
+						ms.Write(buffer.Array, buffer.Offset, result.Count);
+					} while (!result.EndOfMessage);
+
+					if (result.MessageType == WebSocketMessageType.Close)
+						break;
+
+					ms.Seek(0, SeekOrigin.Begin);
+					using (var reader = new StreamReader(ms, Encoding.UTF8))
+						Console.WriteLine(await reader.ReadToEndAsync());
+				}
+			} while (true);
+		}
+
+		static async Task Send(ClientWebSocket socket, string data) =>
+	await socket.SendAsync(Encoding.UTF8.GetBytes(data), WebSocketMessageType.Text, true, CancellationToken.None);
+
+
+		[Serializable]
+        public struct BlocSubscriberSubscription
+		{
+            public string Type, ApiKey, Encoding, Channel;
+            public string[] Markets, Symbols;
+		} 
+	}
+}
+
